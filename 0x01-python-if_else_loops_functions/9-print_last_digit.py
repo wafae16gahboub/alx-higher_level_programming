1d28c504e37b1fe857947158a2f71fb0c3747135
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-06 16:54:29 -0700

    9. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    """Print a string in uppercase."""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("")
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
