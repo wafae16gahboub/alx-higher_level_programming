9c9bb07a5704dc27ca2ac8550f0ac558761f9dfb
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-07 07:14:22 -0700

    tasksss12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-for i in range(1,100,1):
-    y = i // 3 
-    x = (y * 3) - i
-    v = i // 5
-    if x == 0 and ((v * 5) - i) == 0:
-        print("FizzBuzz", end=" ")
-    elif x == 0:
-        print("Fizz", end=" ")
-    elif ((v * 5) - i) == 0:
-        print("Buzz", end=" ")
-    else:
-        print("{}".format(i), end=(" ")) 
+def fizzbuzz():
+    for i in range(1, 100, 1):
+        y = i // 3
+        x = (y * 3) - i
+        v = i // 5
+        if x == 0 and ((v * 5) - i) == 0:
+            print("FizzBuzz", end=" ")
+        elif x == 0:
+            print("Fizz", end=" ")
+        elif ((v * 5) - i) == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(i), end=(" "))
