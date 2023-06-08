5f4fa78f6ee8322a26ba8934f0e15199d34dce60
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-08 12:08:29 -0700

    2.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     else:
         print("{} arguments:".format(y))
         for i in range(1, len(y)):
-            print("{}: {}".format(i, x[n]))
+            print("{}: {}".format(i, x[i]))
