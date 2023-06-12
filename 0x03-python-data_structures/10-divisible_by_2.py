48105f90e5e2b8fdae7499d525c03208b1ccf705
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-12 12:29:58 -0700

    10.

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    the_list = []
+
+    for i in the_list:
+        if i % 2 == 0:
+            the_list.append(True)
+        else:
+            the_list.append(False)
+    return (the_list)
