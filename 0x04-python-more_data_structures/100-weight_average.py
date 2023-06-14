d171c56add8c81958d4203a314ef280121cc0523
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-14 16:00:11 -0700

    13.

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
-        number = 0
-        den = 0
-        for w in my_list:
-            number += (w[0] * w[1])
-            deno += (w[1])
-        return (number/deno)
-    return 0
+    if not my_list:
+        return 0
+
+    number = 0
+    denom = 0
+
+    for w in my_list:
+        number += w[0] * w[1]
+        denom += w[1]
+
+    return (number / denom)
