d50494598086d62470e0497fa154f15ecebfb55f
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-27 17:04:13 -0700

    tasks1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This class contains documentation for class Square"""
+"""__init__ method & instantiation of size after class Square:"""
+
+
 class Square:
     def __init__(self, size):
         self.__size = size
