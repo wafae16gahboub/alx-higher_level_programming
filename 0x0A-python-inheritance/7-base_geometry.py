14f8862997e071996c8a9bd75dcdeb60b8a64fe1
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-07-10 13:28:41 -0700

    7.

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
