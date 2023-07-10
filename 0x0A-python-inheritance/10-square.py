5b2d5ea529ff24e1bf6efa57583d71ea5c6d119f
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-07-10 14:30:07 -0700

    10.

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-Square = __import__('10-square').Square
+"""Defines a Rectangle subclass/child Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
-s = Square(13)
 
-print(s)
-print(s.area())
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
