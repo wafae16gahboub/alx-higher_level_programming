a08e1103f758b0ceb93f784a9007ee124a688e2a
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-07-10 13:17:36 -0700

    2.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-function that returns True if the object is exactly an instance of
-the specified class; otherwise False.
-"""
+"""Exact same object"""
 
 
 def is_same_class(obj, a_class):
+    """is same class"""
     return type(obj) is a_class
