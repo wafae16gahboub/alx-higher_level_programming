f1ad161461b92a364b07ac3de9498108723c5198
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-07-11 14:40:05 -0700

    gggggg

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,4 +12,4 @@ def append_write(filename="", text=""):
         The number of characters appended.
     """
     with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+            return f.write(text)
