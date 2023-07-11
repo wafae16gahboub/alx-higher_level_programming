853887be91c4037ba1a840a313ddf6843568330e
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-07-11 12:19:58 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """function"""
-    with open('filename', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end="")
+    with open(filename, encoding="utf-8") as f:
+        read = f.read()
+        print(read, end="")
