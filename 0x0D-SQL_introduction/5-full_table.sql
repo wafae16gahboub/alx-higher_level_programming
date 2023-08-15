71438cc5877af510a7e0528cf99fa43832716c33
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-08-15 16:59:01 -0700

    tasks5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- script that prints the full description
+SHOW CREATE TABLE first_table
--- 0x0D-SQL_introduction/4-first_table.sql
+++
@@ -1,8 +0,0 @@
---  creates a table called first_table
- CREATE TABLE IF NOT EXISTS  first_table 
-(
-id INT,
-
-
-name VARCHAR(256)
-);
