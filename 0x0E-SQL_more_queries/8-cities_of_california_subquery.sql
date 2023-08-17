81f33243647941d21413713da793dc5d3d3cf11b
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-08-17 13:30:53 -0700

    test

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,4 @@
 -- script that lists all the cities of California that
 -- can be found in the database
 
-SELECT id, name
-FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name='California');
+SELECT  id , name FROM cities where state_id =1;
