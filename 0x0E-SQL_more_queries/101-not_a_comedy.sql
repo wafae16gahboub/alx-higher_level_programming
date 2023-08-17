a082624375cea29c338793226409c19a42794e3c
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-08-16 17:22:28 -0700

    mod

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,13 @@
 --script that lists all shows
+
 SELECT title FROM tv_shows
 WHERE title NOT IN
-(
-SELECT title FROM tv_show_genres AS tvsg
-LEFT JOIN tv_shows AS tvs
-ON tvsg.show_id = tvs.id
-LEFT JOIN tv_genres AS tvg
-ON tvsg.genre_id = tvg.id
-WHERE tvg.name = "Comedy"
-)
+	(
+		SELECT title FROM tv_show_genres AS tvsg
+		LEFT JOIN tv_shows AS tvs
+			ON tvsg.show_id = tvs.id
+		LEFT JOIN tv_genres AS tvg
+			ON tvsg.genre_id = tvg.id
+		WHERE tvg.name = "Comedy"
+	)
 ORDER BY title;
