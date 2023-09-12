754515e4ca4d13511390c4f076cc199a282f91f6
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-12 06:54:39 -0700

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] == null){
- console.log('No argument');
-}else if (process.argv[3]){
- console.log('Argument found');
-}else{
- console.log('Argument found');
+
+if (process.argv[2] == null) {
+  console.log('No argument');
+} else if (process.argv[3]) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
