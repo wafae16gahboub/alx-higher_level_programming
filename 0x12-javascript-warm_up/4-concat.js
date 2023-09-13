bdb4578f2d2f4dd08a7bab9430c1be8ebfb90d7d
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-13 14:03:52 -0700

    fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 if (process.argv[2] == null) {
   console.log('undefined is undefined');
 } else if (process.argv[3]) {
-  console.log(process.argv[2]  + ' is ' + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
   console.log(process.argv[2] + ' is undefined');
 }
