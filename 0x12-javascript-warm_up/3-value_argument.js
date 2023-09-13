50fb02fce3bbc0a589eb1bdfff1d98501feeb46f
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-13 13:44:27 -0700

    tasks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if ( process.argv[2] == null){
-console.log('No argument');
-} else if (process.argv[2]){
-console.log(process.argv[2]);
+if (process.argv[2] == null) {
+  console.log('No argument');
+} else if (process.argv[2]) {
+  console.log(process.argv[2]);
 }
