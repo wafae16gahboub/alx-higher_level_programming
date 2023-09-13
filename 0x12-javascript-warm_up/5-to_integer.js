941437930400a52c126e22cf1f3f2e25ab3c740c
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-13 14:41:19 -0700

    tasks5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-let a = process.argv
-if (isNaN(a[2]) || a[2] == null){
-console.log('Not a number');
-} else { 
-console.log('My number: ' + a[2]);
+const a = process.argv;
+if (isNaN(a[2]) || a[2] == null) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + a[2]);
 }
-
-
