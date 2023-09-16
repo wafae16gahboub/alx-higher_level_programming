d938e6ba8c35c225a39aac465521402f4a7dbb6c
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-16 16:35:39 -0700

    tasks1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,17 +3,20 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           charset="utf8")
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8",
+    )
     cur = conn.cursor()
-    cur.execute("SELECT id, NAME FROM states WHERE NAME LIKE 'N%' COLLATE utf8_bin ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY NAME LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
     conn.close()
-
