843160b32b3a4ed7c0211e547174e2f0d516917f
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-17 14:12:21 -0700

    tasks33

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
+"""script that takes in an argument and displays where name matches"""
+import MySQLdb
+import sys
+
+
+def detectme():
+
+    argvv = sys.argv
+    n = MySQLdb.connect(user=argvv[1],
+                        host='localhost',
+                        port=3306,
+                        passwd=argvv[2],
+                        db=argvv[3])
+
+    veronic = n.cursor()
+    sec = "SELECT*FROM states WHERE BINARY name= %s ORDER BY id ASC"
+    veronic.execute(sec, (argvv[4],))
+    hussa = veronic.fetchall()
+    for i in hussa:
+        print(i)
+    n.close()
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    arg4 = sys.argv[4]
-    cur.execute("SELECT  id, name\
-                FROM states\
-                WHERE name LIKE %s\
-                ORDER BY states.id ASC", (arg4,))
-    query = cur.fetchall()
-    for row in query:
-        print(row)
-    cur.close()
-    db.close()
+    detectme()
