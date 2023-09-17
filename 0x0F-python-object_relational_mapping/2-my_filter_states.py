c5c93257b530f394b72fbb5e17e44a03fbb582ca
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-09-17 09:18:05 -0700

    tasks

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""script that lists all states with a name"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+        ('%' + sys.argv[4] + '%',)
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
